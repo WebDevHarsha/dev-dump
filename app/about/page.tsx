ec4b3d0379b7060c2f81c34e73d189b9e1688a03
WebDevHarsha <vasriharsha@gmail.com>
2025-10-17 17:26:50 +0530

    feat(about): Add About page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import About from '../../components/About'
+import Header from '../../components/Header'
+
+export default function page() {
+  return (
+    <>
+    <Header></Header>
+    <About></About></>
+  )
+}
