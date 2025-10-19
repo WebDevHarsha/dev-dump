df9cfcc7c5955710e44ce6ef8713c0072179f976
WebDevHarsha <vasriharsha@gmail.com>
2025-10-19 14:44:08 +0530

    feat(next.config): Add CloudFront domain for images

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['s3.amazonaws.com', 'images.unsplash.com', 'cdn.discordapp.com'],
+    domains: ['s3.amazonaws.com', 'images.unsplash.com', 'cdn.discordapp.com', 'd112y698adiu2z.cloudfront.net'],
   },
 };
 
