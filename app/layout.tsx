818d005489e4568fef272f9ee7aebb33a7aadcb3
WebDevHarsha <vasriharsha@gmail.com>
2025-10-25 11:40:39 +0530

    feat(branding): Add social media meta & icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,32 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Dev Dump",
+  title: "Dev Dump - All hackathons in one place",
   description: "Because scrolling five sites is overrated.",
+  icons: {
+    icon: "/devdump.png",
+    apple: "/devdump.png",
+  },
+  openGraph: {
+    title: "Dev Dump",
+    description: "Because scrolling five sites is overrated.",
+    images: [
+      {
+        url: "/devdump.png",
+        width: 1200,
+        height: 630,
+        alt: "Dev Dump",
+      },
+    ],
+    siteName: "Dev Dump",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Dev Dump",
+    description: "Because scrolling five sites is overrated.",
+    images: ["/devdump.png"],
+  },
 };
 
 export default function RootLayout({
